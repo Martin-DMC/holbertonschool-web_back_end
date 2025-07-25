639cac1bca7f3e7cc493aa5ae31f5aa5ab3cbba4
Martin-DMC <the.chiperfil@gmail.com>
2025-07-25 10:14:02 -0300

    exercise 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+on this module, i only write some variables with your annotations
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
