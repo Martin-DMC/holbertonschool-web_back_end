93708c7bcf3e23360a1d147457feb46ddf8d76cd
Martin-DMC <the.chiperfil@gmail.com>
2025-07-26 11:14:34 -0300

    arreglo

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 """
+this module contains a function asynchronous
+which return a random number
 """
 import random
+import asyncio
 
 
-async def wait_random(max_delay: int =10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    this function return a float number between 0 until the value get.
+    if nothing value it function use 10 as max deeley
+    """
+    await asyncio.sleep(5)
     return random.uniform(0, max_delay)
