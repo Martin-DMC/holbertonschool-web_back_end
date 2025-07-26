59f19f9a3045d0f26ca621ab644a18453e029f80
Martin-DMC <the.chiperfil@gmail.com>
2025-07-26 12:12:08 -0300

    arreglo

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     this function take the first parameter as limit of elements
     to it list and take the second parameter as the max of deeley.
