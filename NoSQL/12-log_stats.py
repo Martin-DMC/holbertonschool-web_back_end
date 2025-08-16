be86f051cb64aadf11c682bddbf9462fcdded2a3
Martin-DMC <the.chiperfil@gmail.com>
2025-08-16 17:25:11 -0300

    ejercicio 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,29 +10,19 @@ def run_stats():
     """
     Proporciona estadísticas sobre los logs de Nginx en MongoDB.
     """
-    client = None
-    try:
-        client = pymongo.MongoClient()
+    client = pymongo.MongoClient()
+    db = client.logs
+    nginx_collection = db.nginx
 
-        db = client.logs
-        nginx_collection = db.nginx
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
 
-        total_logs = nginx_collection.count_documents({})
-        print(f"{total_logs} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-        print("Methods:")
-        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        for method in methods:
-            count = nginx_collection.count_documents({"method": method})
-            print(f"\tmethod {method}: {count}")
-
-        status_check_count = nginx_collection.count_documents(
-            {"method": "GET", "path": "/status"})
-        print(f"{status_check_count} status check")
-
-    except Exception as e:
-        print(f"Ocurrió un error inesperado: {e}")
-
-    finally:
-        if client:
-            client.close()
+    status_check_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
