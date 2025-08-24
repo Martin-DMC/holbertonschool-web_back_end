c2d36022bfdbe6ec6b68892d6dc767b6cf8d85ec
Martin-DMC <the.chiperfil@gmail.com>
2025-08-24 12:20:54 -0300

    ejercicio 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,20 @@
+export default class Car {
+    constructor(brand, motor, color) {
+        if (typeof brand !== 'string' && brand !== undefined) {
+            throw new TypeError('Brand must be a string')
+        }
+        if (typeof motor !== 'string' && motor !== undefined) {
+            throw new TypeError('Motor must be a string')
+        }
+        if (typeof color !== 'string' && color !== undefined) {
+            throw new TypeError('Color must be a string')
+        }
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    cloneCar() {
+        return new this.constructor();
+    }
+}
