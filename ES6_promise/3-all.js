5caf8b112d4f9b6494917ae57ccc0a78849d40a6
Martin-DMC <the.chiperfil@gmail.com>
2025-08-29 16:14:50 -0300

    fix: arreglo

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup() {
         console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
       })
       .catch(() => {
-        console.log('Singup system offline');
+        console.log('Signup system offline');
       })
 }
