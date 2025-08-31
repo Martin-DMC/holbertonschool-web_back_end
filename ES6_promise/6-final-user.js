e2da4e88d6bd7dfd403471dd2d8d69335eb25e21
Martin-DMC <the.chiperfil@gmail.com>
2025-08-31 02:39:25 -0300

    feat: ejercicio 7 fix: ejercicio 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,7 +5,15 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  const result = await Promise.allSettled([userPromise, photoPromise]);
-
-  return result;
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'rejected') {
+        return {
+          status: 'rejected',
+          value: `Error: ${fileName} cannot be processed`
+        };
+      }
+      return result;
+    });
+  });
 }
