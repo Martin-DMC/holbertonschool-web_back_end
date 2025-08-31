5f33f219d6b0f820378f90557e3fc914efd327a5
Martin-DMC <the.chiperfil@gmail.com>
2025-08-31 02:08:30 -0300

    feat: ejercicio 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const result = await Promise.allSettled([userPromise, photoPromise]);
+
+  return result;
+}
