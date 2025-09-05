ee37b090c83f116299783b07230045c4ac70e18e
Martin-DMC <the.chiperfil@gmail.com>
2025-09-05 14:51:14 -0300

    fix: arreglo

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,17 @@
-const readline = require('readline');
+if (process.stdin.isTTY) {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`)
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+  });
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (answer => {
-    
-    process.stdout.write(`Your name is: ${answer}\n`);
-
-    process.stdout.write('This important software is now closing\n')
-    })
-)
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
