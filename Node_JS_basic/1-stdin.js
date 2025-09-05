0ee84e239ff7cf89a549469a10b080b9394d45f3
Martin-DMC <the.chiperfil@gmail.com>
2025-09-05 15:11:51 -0300

    fix: second change

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,9 +9,7 @@ if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
-  });
-
-  process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
+    process.exit();
   });
 }
