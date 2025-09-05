8d802c60e8591786010d7c0d31ff46d9aee81c0a
Martin-DMC <the.chiperfil@gmail.com>
2025-09-05 14:18:22 -0300

    feat: Using Process stind

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (answer => {
+    
+    process.stdout.write(`Your name is: ${answer}\n`);
+
+    process.stdout.write('This important software is now closing\n')
+    })
+)
