f9b3366598238fe061e575285d30dc4f9de41ff5
Martin-DMC <the.chiperfil@gmail.com>
2025-09-05 15:32:39 -0300

    fix:  new change

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,17 @@
-if (process.stdin.isTTY) {
+async function run() {
+  if (process.stdin.isTTY) {
   process.stdout.write('Welcome to Holberton School, what is your name?\n');
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
+  }
+
+  for await (const chunk of process.stdin) {
+    const name = chunk.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`)
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  });
+    if (process.stdin.isTTY) {
+      process.exit();
+    }
+  }
+
+  process.stdout.write('This important software is now closing\n');
 }
+
+run();
